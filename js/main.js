255bde4f5e80182e1b700ad9ab0c09de15e4267a
alexander weis <privat.alexander.weis@gmail.com>
2022-06-13 15:02:42 +0200

    fix: add float numbers

--- js/main.js
+++ js/main.js
@@ -45,8 +45,7 @@ getRandomInteger(22, 33);
 
 const getRandomIntegerNumber = (firstRangeNumber, lastRangeNumber, floatNumber) => {
   if (firstRangeNumber >= 0 && lastRangeNumber >= 0 && floatNumber >= 0) {
-    firstRangeNumber = Math.ceil(firstRangeNumber);
-    lastRangeNumber = Math.floor(lastRangeNumber);
+
     return Number((Math.random() * (firstRangeNumber - lastRangeNumber) + lastRangeNumber).toFixed(floatNumber));
   }
   throw new Error('Что-то с плавающей запятой у вас не так..., перепроверьте!');
